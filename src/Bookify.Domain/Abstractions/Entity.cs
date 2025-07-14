5c095aa4a6444ac4847ec2e16e636c1ebf548810
Vlad Kovdria <v.kovdria@tabletki.ua>
2025-07-14 18:04:22 +0300

    Define entities; Introduce domain events

---
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,22 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+    
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+    public Guid Id { get; init; }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+    
+    protected void ClearDomainEvent()
+    {
+        _domainEvents.Clear();
+    }
+}
